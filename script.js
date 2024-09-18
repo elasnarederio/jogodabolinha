8735a1d7da5cf12925028b9445fd16251b135a9f
elasnarederio <elasnarederio@gmail.com>
2024-09-18 09:50:37 -0300

    criação de variáveis

---
+++ script.js
@@ -0,0 +1,9 @@
+// Pegando o círculo e a área de jogo
+const circle = document.getElementById('circle');
+const gameArea = document.getElementById('gameArea');
+
+let posX = 50;
+let posY = 50;
+
+const moveSpeed = 10;
+const originalBackgroundColor = '#87CEEB';  // Cor original de fundo
